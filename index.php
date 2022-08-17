4c9cf56178231374ab84b7ad38380359d14c85b9
Diego Roque <diegojoroque@gmail.com>
2022-08-17 06:41:08 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <!--
+		Instituição: Instituto Federal do Sul de Minas Gerais
+		Curso: Técnico de Informática EaD
+		Disciplina: Linguagens de Programação III
+		Tarefa: Semana 3 - Tarefa avaliativa - Organizando e estruturando o site com PHP
+		Aluno: Diego José de Oliveira Roque
+		Data: 19/04/2022
+	-->
+    <head>
+        <meta charset="utf-8">
+        <title>Recomendações de livros - Página inicial</title>
+        <link rel="stylesheet" href="estilo.css" type="text/css">
+    </head>
+    <body>
+        <?php
+            include("topo.php");        
+        ?>
+
+        <main>
+            <section>
+                <p align="justify">A busca pelo conhecimento é fascinante, principalmente acerca de coisas que realmente me interessam!<br> Os livros são ferramentas incríveis que podem nos ajudar e até guiar nessa jornada. E é fato que alguns deles marcam nossa vida.<br> Pensando nisso, quero deixar algumas sugestões de livros que me ensinaram (e ainda ensinam) muito.</p>
+            </section>
+        </main>
+
+        <?php
+            include("rodape.php");        
+        ?>        
+    </body>
+</html>
